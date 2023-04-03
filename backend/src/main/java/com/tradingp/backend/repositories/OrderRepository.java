ff176e7c7e770c75f747252332ce3c21385f3c7f
cencorroll <aharundd@gmail.com>
2023-04-03 10:52:58 +0100

    adding user

--- backend/src/main/java/com/tradingp/backend/repositories/OrderRepository.java
+++ backend/src/main/java/com/tradingp/backend/repositories/OrderRepository.java
@@ -3,11 +3,9 @@ package com.tradingp.backend.repositories;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import com.tradingp.backend.entities.Order;
 
-@Repository
 public interface OrderRepository extends JpaRepository<Order, Integer>{
 
   List<Order> findByOrderType(String orderType);
